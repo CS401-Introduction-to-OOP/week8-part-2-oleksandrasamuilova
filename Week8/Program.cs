07c11de4b86e003da2d9ad2e59ada0fa9ed22c3f
Oleksandra Samuilova <oleksandra.samuilova.25@kse.org.ua>
2026-03-28 23:41:20 +0200

    Stage 3

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,18 @@
-﻿
-class Program
+﻿using System;
+using Variant1;
+
+var file = new FileResource("dfjndk.txt");
+var network = new NetworkResource("kmvlsadld");
+
+var manager = new ResourceManager<Resource>();
+
+manager.Add(file);
+manager.Add(network);
+
+manager.OpenAll();
+using (var tempFile = new FileResource("tempFile.csv"))
 {
-    public static void Main()
-    {
-        
-    }
-    
-}
+    tempFile.Open();
+}    
+
+manager.CloseAll();
